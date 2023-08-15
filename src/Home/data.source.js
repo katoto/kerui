90c9cb4ef5b7b9de8c6ec9fa20af1153cd85f44d
zg.xie <zg.xie@huolala.cn>
2023-08-15 16:15:29 +0800

    feature: init tech

---
+++ src/Home/data.source.js
@@ -0,0 +1,463 @@
+import React from 'react';
+import Img from '@/image/logo2.png';
+
+// http://www.sanjiedq.com/editor/attached/image/20190813/20190813164352_36699.jpg
+
+export const Nav30DataSource = {
+  wrapper: { className: 'header3 home-page-wrapper' },
+  page: { className: 'home-page llbyiegnx9a-editor_css' },
+  logo: {
+    className: 'header3-logo',
+    children: Img || 'https://gw.alipayobjects.com/zos/basement_prod/b30cdc2a-d91c-4c78-be9c-7c63b308d4b3.svg',
+  },
+  Menu: {
+    className: 'header3-menu',
+    children: [
+      {
+        name: 'item1',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '首页', name: 'text' }],
+        },
+      },
+      {
+        name: 'item2',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '成功案例', name: 'text' }],
+        },
+      },
+      {
+        name: 'item3',
+        className: 'header3-item',
+        children: {
+          href: '#',
+          children: [{ children: '关于我们', name: 'text' }],
+        },
+      },
+    ],
+  },
+  mobileMenu: { className: 'header3-mobile-menu' },
+};
+export const Banner20DataSource = {
+  wrapper: { className: 'banner2' },
+  BannerAnim: {
+    children: [
+      {
+        name: 'elem0',
+        BannerElement: { className: 'banner-user-elem' },
+        page: { className: 'home-page banner2-page' },
+        textWrapper: { className: 'banner2-text-wrapper' },
+        bg: { className: 'bg bg0' },
+        title: { className: 'banner2-title', children: '宿州科瑞电器' },
+        content: {
+          className: 'banner2-content',
+          children: '从事煤矿电子产品的研制开发、生产、销售和服务为一体的创新企业',
+        },
+        button: { className: 'banner2-button', children: '了解更多' },
+      },
+    ],
+  },
+};
+export const Feature00DataSource = {
+  wrapper: { className: 'home-page-wrapper content0-wrapper' },
+  page: { className: 'home-page content0' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: '产品与服务' }],
+  },
+  childWrapper: {
+    className: 'content0-block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/WBnVOjtIlGWbzyQivuyq.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式购齐煤矿电子产品',
+            },
+            { name: 'content', children: '支付、结算便捷，降成本' },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/YPMsLQuCEXtuEkmXTTdk.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式风险监控',
+            },
+            {
+              name: 'content',
+              children: '在所有需求配置环节事前风险控制和质量控制能力',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'content0-block',
+        md: 8,
+        xs: 24,
+        children: {
+          className: 'content0-block-item',
+          children: [
+            {
+              name: 'image',
+              className: 'content0-block-icon',
+              children:
+                'https://zos.alipayobjects.com/rmsportal/EkXWVvAaFJKCzhMmQYiX.png',
+            },
+            {
+              name: 'title',
+              className: 'content0-block-title',
+              children: '一站式数据运营',
+            },
+            {
+              name: 'content',
+              children: '以管理出效益、促发展',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Feature60DataSource = {
+  wrapper: { className: 'home-page-wrapper feature6-wrapper' },
+  OverPack: { className: 'home-page feature6', playScale: 0.3 },
+  Carousel: {
+    className: 'feature6-content',
+    dots: false,
+    wrapper: { className: 'feature6-content-wrapper' },
+    titleWrapper: {
+      className: 'feature6-title-wrapper',
+      barWrapper: {
+        className: 'feature6-title-bar-wrapper',
+        children: { className: 'feature6-title-bar' },
+      },
+      title: { className: 'feature6-title' },
+    },
+    children: [
+      {
+        title: { className: 'feature6-title-text', children: '服务指标' },
+        className: 'feature6-item',
+        name: 'block0',
+        children: [
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child0',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '千' },
+              toText: true,
+              children: '116',
+            },
+            children: { className: 'feature6-text', children: '服务用户' },
+          },
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child1',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '万' },
+              toText: true,
+              children: '1.17',
+            },
+            children: { className: 'feature6-text', children: '产品销量数' },
+          },
+          {
+            md: 8,
+            xs: 24,
+            className: 'feature6-number-wrapper',
+            name: 'child2',
+            number: {
+              className: 'feature6-number',
+              unit: { className: 'feature6-unit', children: '百' },
+              toText: true,
+              children: '2.10',
+            },
+            children: { className: 'feature6-text', children: '模型样品数量' },
+          },
+        ],
+      }
+    ],
+  },
+};
+export const Content50DataSource = {
+  wrapper: { className: 'home-page-wrapper content5-wrapper' },
+  page: { className: 'home-page content5' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      { name: 'title', children: '成熟产品案例', className: 'title-h1' },
+      {
+        name: 'content',
+        className: 'title-content',
+        children: '专业从事煤矿电子产品的研制开发、生产、销售和服务为一体的创新企业',
+      },
+    ],
+  },
+  block: {
+    className: 'content5-img-wrapper',
+    gutter: 16,
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814175747_82336.png',
+          },
+          content: { children: '矿用通讯信号系列' },
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814180400_32848.png',
+          },
+          content: { children: '声光语言报警器系列' },
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814181219_99978.jpg',
+          },
+          content: { children: '皮带机保护及控制系列' },
+        },
+      },
+      {
+        name: 'block3',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814182105_93641.png',
+          },
+          content: { children: '扩播电话系列' },
+        },
+      },
+      {
+        name: 'block4',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814182249_84781.png',
+          },
+          content: { children: '洒水降尘控制系列' },
+        },
+      },
+      {
+        name: 'block5',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190820102849_30184.png',
+          },
+          content: { children: '传感器系列' },
+        },
+      },
+      {
+        name: 'block6',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814181448_89274.png',
+          },
+          content: { children: '隔爆型LED照明系列' },
+        },
+      },
+      {
+        name: 'block7',
+        className: 'block',
+        md: 6,
+        xs: 24,
+        children: {
+          wrapper: { className: 'content5-block-content' },
+          img: {
+            children:
+              'http://www.sanjiedq.com/editor/attached/lehome_thumb/20190814182441_95116.jpg',
+          },
+          content: { children: '隔爆型LED照明系列' },
+        },
+      },
+    ],
+  },
+};
+export const Teams10DataSource = {
+  wrapper: { className: 'home-page-wrapper teams1-wrapper' },
+  page: { className: 'home-page teams1' },
+  OverPack: { playScale: 0.3, className: '' },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [{ name: 'title', children: '团队成员' }],
+  },
+  block: {
+    className: 'block-wrapper',
+    children: [
+      {
+        name: 'block0',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*--rVR4hclJYAAAAAAAAAAABjARQnAQ',
+            },
+            { name: 'title', className: 'teams1-title', children: '叶秀英' },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: '公司+职位 信息暂缺',
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children:
+                'AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、无限可能的数据可视化最佳实践。',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*njqxS5Ky7CQAAAAAAAAAAABjARQnAQ',
+            },
+            { name: 'title', className: 'teams1-title', children: '张云飞' },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: '公司+职位 监事',
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children:
+                '担任宿州科锐电器有限公司高管。',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        className: 'block',
+        md: 8,
+        xs: 24,
+        titleWrapper: {
+          children: [
+            {
+              name: 'image',
+              className: 'teams1-image',
+              children:
+                'https://gw.alipayobjects.com/mdn/rms_ae7ad9/afts/img/A*--rVR4hclJYAAAAAAAAAAABjARQnAQ',
+            },
+            { name: 'title', className: 'teams1-title', children: '叶秀英' },
+            {
+              name: 'content',
+              className: 'teams1-job',
+              children: '公司+职位 信息暂缺',
+            },
+            {
+              name: 'content1',
+              className: 'teams1-content',
+              children:
+                'AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、无限可能的数据可视化最佳实践。',
+            },
+          ],
+        },
+      },
+    ],
+  },
+};
+export const Footer00DataSource = {
+  wrapper: { className: 'home-page-wrapper footer0-wrapper' },
+  OverPack: { className: 'home-page footer0', playScale: 0.05 },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        Copyright © 2023
+        <a href="https://beian.miit.gov.cn/">宿州科锐电器有限公司</a>
+        All Rights Reserved
+      </span>
+    ),
+  },
+};
